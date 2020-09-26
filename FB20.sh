b9e3dd1981361570bbe2cca80b2c9c57bbe7c41f
mrx-family <71932023+mrx-family@users.noreply.github.com>
2020-09-26 23:42:03 +0700

    Add files via upload

---
+++ FB20.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+clear
+sleep 5
+echo "> masukkan id target"
+read target
+sleep 5
+echo "memeriksa..."
+sleep 10
+echo "memulai crack password..."
+sleep 5
+echo "please wait..."
+sleep 60
+echo "password ditemukan!!"
+sleep 5
+echo "id:100051760936510"
+sleep 5
+echo "password:raraQ123loveyou"
+sleep 5
+
